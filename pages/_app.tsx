f3f6297b57e79c58e67c48fe69f2d7562a7b0855
Steven Tey <stevensteel97@gmail.com>
2022-03-19 12:34:54 -0700

    added tailwind, typescript, prisma, next-auth

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import "@/styles/globals.css";
+import { AppProps } from "next/app";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    // ...add more providers here
+  ],
+});
