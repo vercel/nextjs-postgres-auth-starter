d35f6943890ea758f4a9ba5e6a258ca694c2d0dd
Steven Tey <stevensteel97@gmail.com>
2022-11-28 19:25:31 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         <div className="flex space-x-3">
           <Link
             href="/protected"
-            prefetch={false}
             className="text-stone-400 hover:text-stone-200 transition-all"
           >
             Protected Page
