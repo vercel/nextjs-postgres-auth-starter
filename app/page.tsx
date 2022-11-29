c76e65bfe75cb1d7276c5887dd90b8c78208c399
Steven Tey <stevensteel97@gmail.com>
2022-11-29 17:21:38 -0600

    updated copy

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,38 @@ export default function Home() {
           alt="Platforms on Vercel"
           className="w-48 h-48"
         />
+        <div className="text-center max-w-screen-sm mb-10">
+          <h1 className="text-stone-200 font-bold text-2xl">
+            Next.js MySQL Auth Starter
+          </h1>
+          <p className="text-stone-400 mt-5">
+            This is a{" "}
+            <a
+              href="https://nextjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-stone-400 underline hover:text-stone-200 transition-all"
+            >
+              Next.js
+            </a>{" "}
+            starter kit that uses{" "}
+            <a
+              href="https://next-auth.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-stone-400 underline hover:text-stone-200 transition-all"
+            >
+              Next-Auth
+            </a>{" "}
+            for simple email + password login and a MySQL database to persist
+            the data.
+          </p>
+        </div>
         <div className="flex space-x-3">
           <Link
             href="/protected"
-            prefetch={false}
-            className="text-stone-400 hover:text-stone-200 transition-all"
+            prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
+            className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
             Protected Page
           </Link>
@@ -25,7 +52,7 @@ export default function Home() {
             href="https://github.com/steven-tey/nextjs-typescript-starter"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-stone-400 hover:text-stone-200 transition-all"
+            className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
             GitHub
           </a>
--- components/toaster.tsx
+++ components/toaster.tsx
@@ -1,3 +1,15 @@
 "use client";
 
-export { Toaster as default } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { Toaster as ToasterClient } from "react-hot-toast";
+
+export default function Toaster() {
+  // workaround for react hydration error: https://nextjs.org/docs/messages/react-hydration-error
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsSSR(false);
+    }
+  }, []);
+  return isSSR ? null : <ToasterClient />;
+}
