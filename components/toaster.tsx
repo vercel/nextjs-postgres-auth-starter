02ccec9b49f670fc4cdf1af5fc3439b5d60ec352
Steven Tey <stevensteel97@gmail.com>
2022-11-30 13:24:13 -0600

    Update toaster.tsx

--- components/toaster.tsx
+++ components/toaster.tsx
@@ -1,13 +1,3 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Toaster as ToasterClient } from "react-hot-toast";
-
-export default function Toaster() {
-  // workaround for react hydration error: https://nextjs.org/docs/messages/react-hydration-error
-  const [isSSR, setIsSSR] = useState(true);
-  useEffect(() => {
-    setIsSSR(false);
-  }, []);
-  return isSSR ? null : <ToasterClient />;
-}
+export { Toaster as default } from "react-hot-toast";
