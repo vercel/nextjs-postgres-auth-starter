bb2342305e546f56a0d37f958dcad6a6267170f1
九旬 <1160030724@qq.com>
2023-06-02 14:38:11 +0800

    fix: getServerSession replace unstable_getServerSession

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -1,7 +1,7 @@
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 
 export default async function AuthStatus() {
-  const session = await unstable_getServerSession();
+  const session = await getServerSession();
   return (
     <div className="absolute top-5 w-full flex justify-center items-center">
       {session && (
