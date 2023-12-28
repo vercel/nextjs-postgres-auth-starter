d98f61526585be7aab663d957093deacac8441d2
Lee Robinson <lrobinson2011@gmail.com>
2023-12-28 13:44:32 -0600

    Update

--- app/auth.ts
+++ app/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { compare } from 'bcryptjs';
+import { compare } from 'bcrypt-ts';
 import { getUser } from 'app/db';
 import { authConfig } from 'app/auth.config';
 
--- app/db.ts
+++ app/db.ts
@@ -2,8 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 import { eq } from 'drizzle-orm';
 import postgres from 'postgres';
-import { genSaltSync, hashSync } from 'bcryptjs';
+import { genSaltSync, hashSync } from 'bcrypt-ts';
 
+// Optionally, if not using email/pass login, you can
+// use the Drizzle adapter for Auth.js / NextAuth
+// https://authjs.dev/reference/adapter/drizzle
 let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
 let db = drizzle(client);
 
